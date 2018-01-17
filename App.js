bb46be548c512e24bf2b45b2c16ac024afbf02b2
unknown <yjeong19@gmail.com>
2018-01-16 21:59:34 -0500

    decision section

--- App.js
+++ App.js
@@ -3,13 +3,15 @@ import { View } from 'react-native';
 import { Container, Button } from 'native-base';
 import HeaderComponent from './src/Components/HeaderComponent'
 import LandingPage from './src/Components/LandingPage'
+import DecisionSection from './src/Components/DecisionSection'
 
 export default class App extends React.Component {
   render() {
     return (
       <Container style={{ backgroundColor: '#e35141' }}>
         <HeaderComponent />
-        <LandingPage />
+        {/* <LandingPage /> */}
+        <DecisionSection />
       </Container>
     );
   }
