9d27916bc88c7876a6c024a108806e1116a248e5
Donald Joy <djsmacbookpro-2017@Donalds-MBP.fios-router.home>
2018-01-16 19:07:02 -0500

    landing page incorporated into app

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { View } from 'react-native';
+import { Container, Button } from 'native-base';
 import HeaderComponent from './src/Components/HeaderComponent'
+import LandingPage from './src/Components/LandingPage'
 
 export default class App extends React.Component {
   render() {
     return (
-      <View>
+      <Container style={{ backgroundColor: '#e35141' }}>
         <HeaderComponent />
-        
-      </View>
+        <LandingPage />
+      </Container>
     );
   }
 }
