2b07899e82090f8e6cdb1c131fdc0b90d50a233c
Donald Joy <djsmacbookpro-2017@Donalds-MacBook-Pro.local>
2018-01-18 09:51:29 -0500

    add yes or no buttons to decisionSection

--- modules/main/components/DecisionSection/DecisionSection.js
+++ modules/main/components/DecisionSection/DecisionSection.js
@@ -1,64 +1,61 @@
 import React, {Component} from 'react';
-import {Image, StyleSheet} from 'react-native';
+import {Image, StyleSheet, Alert } from 'react-native';
 import { Container, Header, Content, Card, CardItem, Thumbnail, Title,Text, Button, Icon, Left, Body, Right } from 'native-base';
 import API from '../../../../Utils/API'
 
 
 
 export default class DecisionSection extends Component{
-    
+
     render(){
 
         const styles = StyleSheet.create({
             image: {
                 flex: 1,
-                // NEED TO FIND A WAY TO MAKE WIDTH 100% SCREEN 
+                // NEED TO FIND A WAY TO MAKE WIDTH 100% SCREEN
                 width: 320,
             }
         });
         const test = API.getRestaurant;
         const DecisionImage = this.props.image
-        console.log(test,"righht here");
+        console.log(test,"right here");
         return(
-            <Container>
-                <Content>
-                    <Card style={styles.image}>
-                        <CardItem>
-                            <Left>
-                                <Thumbnail source={{uri: DecisionImage}} />
-                                <Body>
-                                    {/* THIS IS WHERE THE RESTAURANT NAME SHOULD GO */}
-                                    <Text>NativeBase</Text>
-                                    {/* THIS IS WHRERE MAYBE RESTAURAND LOCATION? */}
-                                    <Text note>GeekyAnts</Text>
-                                </Body>
-                            </Left>
-                        </CardItem>
-                        <CardItem cardBody>
-                            <Image source={{uri: DecisionImage}} style={{height: 200, width: 200, flex: 1}}/>
-                        </CardItem>
-                        <CardItem>
-                            <Left>
-                                <Button transparent>
-                                    <Icon active name="thumbs-up" />
-                                    {/* this might be state */}
-                                    <Text>12 Likes</Text>
-                                </Button>
-                            </Left>
-                            <Body>
-                                <Button transparent>
-                                    <Icon active name='chevron-thin-down' />
-                                    <Text>4 Comments</Text>
-                                </Button>
-                            </Body>
-                            <Right>
-                                {/* what do we want here*/}
-                                <Text>11h ago</Text>
-                            </Right>
-                        </CardItem>
-                    </Card>
-                </Content>
-            </Container>
+          <Container>
+            <Content>
+              <Card style={styles.image}>
+                <CardItem>
+                  <Left>
+                    <Thumbnail source={{uri: DecisionImage}} />
+                    <Body>
+                        {/* THIS IS WHERE THE RESTAURANT NAME SHOULD GO */}
+                        <Text>Restaurant Name</Text>
+                        {/* THIS IS WHRERE MAYBE RESTAURAND LOCATION? */}
+                        <Text note>GeekyAnts</Text>
+                    </Body>
+                  </Left>
+              </CardItem>
+              <CardItem cardBody>
+                  <Image source={{uri: DecisionImage}} style={{height: 200, width: 200, flex: 1}}/>
+              </CardItem>
+              <CardItem>
+                <Left>
+                  <Button style={{backgroundColor: "red"}} onPress={() => {Alert.alert('NO!');}}>
+                    <Icon name='thumbs-down' />
+                  </Button>
+
+                </Left>
+                <Body>
+                  <Text>Do you want to eat here?</Text>
+                </Body>
+                <Right>
+                  <Button style={{backgroundColor: "green"}} onPress={() => {Alert.alert('MMMM Yes!');}}>
+                    <Icon name='thumbs-up' />
+                  </Button>
+                </Right>
+                </CardItem>
+              </Card>
+            </Content>
+          </Container>
         )
     }
 }
