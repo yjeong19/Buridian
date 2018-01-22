9de41a8093cbfa4d81f66324e540a68ea20cc233
unknown <yjeong19@gmail.com>
2018-01-22 14:59:31 -0500

    Youngs-branch

--- modules/main/scenes/Form.js
+++ modules/main/scenes/Form.js
@@ -34,15 +34,6 @@ class Home extends React.Component {
         console.log("login successful...Form page loaded");
       }
 
-      handleAPI = ()=>{
-        API.getRestaurant()
-        .then((res)=> res.json())
-        .then((resJson)=>{
-          // console.log(resJson);
-        })
-
-      }
-
     onSignOut() {
       this.props.signOut(this.onSuccess.bind(this), this.onError.bind(this))
     }
@@ -55,47 +46,31 @@ class Home extends React.Component {
       Alert.alert('Oops!', error.message);
     }
 
-    handleRandomizeButton = (category, near)=>{
-      API.getRestaurant(category, near)
-      .then((response)=> response.json())
-      .then((data)=>{
-        // let data = JSON.parse(response._bodyInit);
-        let i = Math.floor((Math.random() * data.response.venues.length) + 1);
-        this.setState({restaurantName: data.response.venues[i].name});
-        this.setState({address: data.response.venues[i].location.formattedAddress[0]});
-        this.setState({phone: data.response.venues[i].contact.formattedPhone});
-        this.setState({website: data.response.venues[i].url});
-        this.setState({restaurantId: data.response.venues[i].id});
-        this.setState({fourSquarePage: data.response.venues[i].menu.url});
-        // console.log(data.response.venues[i]);
-        // console.log("4sq url" + data.response.venues[i].menu.url);
-        this.handlePhoto();
-      });
-    }
-
-    handlePhoto=()=>{
-      API.getPhoto(this.state.restaurantId)
-      .then((response)=> response.json())
-      .then((responseJson)=>{
-        if(!responseJson.response.photos.items){
-          this.setState({imageUrl: 'http://lorempicsum.com/futurama/350/200/1'})
-          console.log(this.state.url)
-        }
-        else{
-          const photoObject = responseJson.response.photos.items[0];
-          // let imageUrl = photoObject.map(photos =>{
-          //   return photos.prefix + '300x500' + photos.suffix;
-          // })
-          /// this gives an array of links
-
-          let imageUrl = photoObject.prefix + '300x500' + photoObject.suffix
 
 
-          console.log(imageUrl)
-          this.setState({imageUrl})
-        }
-      })
-    }
+    // handlePhoto=()=>{
+    //   API.getPhoto(this.state.restaurantId)
+    //   .then((response)=> response.json())
+    //   .then((responseJson)=>{
+    //     if(!responseJson.response.photos.items){
+    //       this.setState({imageUrl: 'http://lorempicsum.com/futurama/350/200/1'})
+    //       console.log(this.state.url)
+    //     }
+    //     else{
+    //       const photoObject = responseJson.response.photos.items[0];
+    //       // let imageUrl = photoObject.map(photos =>{
+    //       //   return photos.prefix + '300x500' + photos.suffix;
+    //       // })
+    //       /// this gives an array of links
+
+    //       let imageUrl = photoObject.prefix + '300x500' + photoObject.suffix
+
+
+    //       console.log(imageUrl)
+    //       this.setState({imageUrl})
+    //     }
+    //   })
+    // }
 
     render() {
       return (
