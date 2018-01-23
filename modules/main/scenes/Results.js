841d26b5d5854446b1f07cd258fa5dc18e5ae8e4
unknown <yjeong19@gmail.com>
2018-01-23 04:09:15 -0500

    deckslider

--- modules/main/scenes/Results.js
+++ modules/main/scenes/Results.js
@@ -29,69 +29,72 @@ class Results extends React.Component {
         fourSquarePage: "",
         numOptions: "",
         categoryId: [],
+        objArr: [],
         images: ["https://media.boingboing.net/wp-content/uploads/2017/03/surprised-cat-04.jpg", "https://www.hobartcity.com.au/files/assets/public/emergency-management/cat_and_dog-1024x899.jpg?w=1200"]
       }
 
       componentDidMount(){      
-        this.handleRandomizeButton();
+        // this.handleRandomizeButton();
         // this.handlePhoto();
       }
 
-      handlePhoto=()=>{
-        // console.log("this.props.placeholderImage: " + this.props.placeholderImage);
-        // this.setState({imageUrl: this.props.placeholderImage})
-        API.getPhoto(this.state.restaurantId)
-        .then((response)=> response.json())
-        .then((responseJson)=>{
-              // let imageUrl = photoObject.map(photos =>{
-          //   return photos.prefix + '300x500' + photos.suffix;
-          // })
-          /// this gives an array of links
-          // console.log("responseJson: " + responseJson.response.photos.items[0]);
-          console.log("this.placeholderImage: " + this.props.placeholderImage);
-          const photoObject = responseJson.response.photos.items[0];
-          let imageUrl = photoObject.prefix + '300x500' + photoObject.suffix;
+      // handlePhoto=()=>{
+      //   // console.log("this.props.placeholderImage: " + this.props.placeholderImage);
+      //   // this.setState({imageUrl: this.props.placeholderImage})
+      //   API.getPhoto(this.state.restaurantId)
+      //   .then((response)=> response.json())
+      //   .then((responseJson)=>{
+      //         // let imageUrl = photoObject.map(photos =>{
+      //     //   return photos.prefix + '300x500' + photos.suffix;
+      //     // })
+      //     /// this gives an array of links
+      //     // console.log("responseJson: " + responseJson.response.photos.items[0]);
+      //     console.log("this.placeholderImage: " + this.props.placeholderImage);
+      //     const photoObject = responseJson.response.photos.items[0];
+      //     let imageUrl = photoObject.prefix + '300x500' + photoObject.suffix;
          
-          responseJson.response.photos.items[0] ? this.setState({imageUrl}) : this.setState({imageUrl: 'http://lorempicsum.com/futurama/350/200/1'})
-        })
-      }
+      //     responseJson.response.photos.items[0] ? this.setState({imageUrl}) : this.setState({imageUrl: 'http://lorempicsum.com/futurama/350/200/1'})
+      //   })
+      // }
          
 
       handleSettingsPress = ()=>{
         Alert.alert("settings");
       }
 
-      handleRandomizeButton = ()=>{
-        const categories = []
-        this.props.categoryObj.map(category=>{
-          console.log(category, 'map category')
-          categories.push(category.categoryID);
-        })
+      // handleRandomizeButton = ()=>{
+      //   const categories = []
+      //   this.props.categoryObj.map(category=>{
+      //     console.log(category, 'map category')
+      //     categories.push(category.categoryID);
+      //   })
 
         
         // console.log(this.props.categoryObj[0].categoryID, this.props.location)
-        API.getRestaurant(categories.join(), this.props.location)
-        .then((response)=> response.json())
-        .then((data)=>{
-          // console.log(data, '------------line 79')
-          // let data = JSON.parse(response._bodyInit);
-          let i = Math.floor((Math.random() * data.response.venues.length) + 1);
-          this.setState({restaurantName: data.response.venues[i].name});
-          this.setState({address: data.response.venues[i].location.formattedAddress[0] ? data.response.venues[i].location.formattedAddress[0] : 'testing' });
-          this.setState({phone: data.response.venues[i].contact.formattedPhone});
-          this.setState({website: data.response.venues[i].url ? data.response.venues[i].url : 'https://www.fousquare.com'});
-          //operator below needs to be changed for else
-          this.setState({restaurantId: data.response.venues[i].id ? data.response.venues[i].id :'4b0df699f964a520345323e3' });
-          this.setState({fourSquarePage: data.response.venues[i].menu.url ? data.response.venues[i].menu.url : 'https://www.foursquare.com'});
-          // console.log(data.response.venues[i]);
-          // console.log("4sq url" + data.response.venues[i].menu.url);
-          this.handlePhoto();
-          // console.log(this.props)
+      //   API.getRestaurant(categories.join(), this.props.location)
+      //   .then((response)=> response.json())
+      //   .then((data)=>{
+      //     // console.log(data.response.venues, '------------line 79')
+
+      //     this.setState({objArr: data.response.venues})
+      //     // let data = JSON.parse(response._bodyInit);
+      //     let i = Math.floor((Math.random() * data.response.venues.length) + 1);
+      //     this.setState({restaurantName: data.response.venues[i].name});
+      //     this.setState({address: data.response.venues[i].location.formattedAddress[0] ? data.response.venues[i].location.formattedAddress[0] : 'testing' });
+      //     this.setState({phone: data.response.venues[i].contact.formattedPhone});
+      //     this.setState({website: data.response.venues[i].url ? data.response.venues[i].url : 'https://www.fousquare.com'});
+      //     //operator below needs to be changed for else
+      //     this.setState({restaurantId: data.response.venues[i].id ? data.response.venues[i].id :'4b0df699f964a520345323e3' });
+      //     this.setState({fourSquarePage: data.response.venues[i].menu.url ? data.response.venues[i].menu.url : 'https://www.foursquare.com'});
+      //     // console.log(data.response.venues[i]);
+      //     // console.log("4sq url" + data.response.venues[i].menu.url);
+      //     this.handlePhoto();
+      //     // console.log(this.props)
           
-        });
-        // console.log('line 68 ===================', categories, 'end ==========')
-        // console.log(this.props);
-      }
+      //   });
+      //   // console.log('line 68 ===================', categories, 'end ==========')
+      //   // console.log(this.props);
+      // }
 
       handleYesButton = () => {
         Linking.openURL(this.state.fourSquarePage).catch(err => console.error('An error occurred', err));
@@ -123,10 +126,11 @@ class Results extends React.Component {
           backgroundColor: '#e35141',
           justifyContent: "center",
           alignItems: "center"}}>
-        <ScrollView style={{flex:1}}>
+        {/* <ScrollView style={{flex:1}}> */}
           <DecisionSection
             image={this.state.imageUrl}
             style={{flex: 1}}
+            location={this.props.location}
             randomized={this.handleRandomizeButton}
             restaurantName={this.state.restaurantName}
             address={this.state.address}
@@ -134,8 +138,10 @@ class Results extends React.Component {
             website={this.state.website}
             fourSquarePage={this.handleYesButton}
             handleImagePress={this.handleImagePress}
+            objArr = {this.props.categoryObj}
+            categoryObj = {this.props.categoryObj}
           />
-        </ScrollView>
+        {/* </ScrollView> */}
           <Button
             raised
             title={'LOG OUT'}
